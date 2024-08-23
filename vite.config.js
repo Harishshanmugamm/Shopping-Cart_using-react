ff5a0ef2d9e9b2c973cc34a4c6ec76676020ecae
Harishshanmugamm <harishshanmugamm@outlook.com>
2024-08-23 17:20:13 +0530

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/Shopping-Cart_using-react",
+  base: "/Shopping-Cart_using-react",
   plugins: [react()],
-})
+});
